cbc811d7e76010097b51a0c7852449cf5620cf78
Jens Schauder <jens@schauderhaft.de>
2022-12-13 10:50:51 +0100

    Adds a test demonstrating that the problem is one of time zones, postgres and Hibernate, not of Spring Data

--- src/test/java/com/example/lastmodifiedproblem/LastmodifiedproblemApplicationTests.java
+++ src/test/java/com/example/lastmodifiedproblem/LastmodifiedproblemApplicationTests.java
@@ -1,8 +1,13 @@
 package com.example.lastmodifiedproblem;
 
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Instant;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +16,9 @@ class LastmodifiedproblemApplicationTests {
 
     @Autowired
     private BlogPostRepository blogPostRepository;
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void shouldUpdateLastModifiedDateCorrectly() {
@@ -32,4 +40,20 @@ class LastmodifiedproblemApplicationTests {
         assertThat(updatedPost.getUpdatedAt()).isAfter(savedPost.getUpdatedAt());
     }
 
+    @Test
+    @Transactional
+    void persistsAndLoadsInstant() {
+        EntityWithInstant entity = new EntityWithInstant();
+        entity.id = UUID.randomUUID();
+        entity.instant = Instant.now();
+
+        em.persist(entity);
+
+        em.flush();
+        em.clear();
+
+        EntityWithInstant reloaded = em.find(EntityWithInstant.class, entity.id);
+
+        assertThat(entity.instant).isEqualTo(reloaded.instant);
+    }
 }
